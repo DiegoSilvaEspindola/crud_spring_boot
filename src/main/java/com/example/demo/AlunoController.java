23d46441f783351a93b4ecd173199aa327734a9c
Diego <diegosilvaespindola@gmail.com>
2022-02-12 21:41:32 -0300

    commit correcao

--- src/main/java/com/example/demo/AlunoController.java
+++ src/main/java/com/example/demo/AlunoController.java
@@ -8,51 +8,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/trycatch")
+@RequestMapping("/alunos")
 
 public class AlunoController {
-
     private AlunoService alunoService;
-
     public AlunoController(AlunoService alunoService) {
         this.alunoService = alunoService;
     }
-
     List<Aluno> alunos = new ArrayList<>();
-
-
     @GetMapping
-        public List<Aluno> findAll (@RequestParam (required = false) Integer id, String nome) {
-
-        if(id!= null){
-            return alunoService.getIdAluno(id);
-        }
-        if(nome!= null){
-            return alunoService.getNameAluno(nome);
-        }
-
+    public List<Aluno> findAll () {
         return alunoService.getAll();
     }
-
-
-    /*@PostMapping
-    public void add(@RequestBody final Aluno aluno){
-        alunoService.adiciona(aluno);
-    }*/
-
+    @GetMapping
+    public List<Aluno>getByName(@RequestBody Aluno aluno){
+        return alunoService.getNameAluno(aluno);
+    }
+    @GetMapping
+    public List<Aluno>getById(@RequestParam Aluno aluno){
+        return alunoService.getIdAluno(aluno);
+    }
     @PostMapping
     public ResponseEntity<Integer> add(@RequestBody final Aluno aluno){
         alunoService.adiciona(aluno);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
-
-
     @PutMapping
     public void update(@RequestBody final Aluno aluno){
         alunoService.alterar(aluno);
 
     }
-
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Integer id){
         alunoService.deletando(id);
